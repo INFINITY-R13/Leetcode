de7c482737c5513f3f9beeb16ae3deb83a1eb735
Raghuraj Singh <130720974+INFINITY-R13@users.noreply.github.com>
2026-01-05 12:58:53 +0530

    L1
    Added test cases for the twoSum function.

--- leetcode1.py
+++ leetcode1.py
@@ -5,11 +5,15 @@ You can return the answer in any order."""
 
 
 def twoSum(nums, target):
-    num_to_index = {}
-    for index, num in enumerate(nums):
-        required = target - num
-        if required in num_to_index:
-            return [num_to_index[required], index]
-        num_to_index[num] = index
-        
-    return []  # In case there is no solution, though the problem guarantees one.
+       num_to_index = {}
+       for index, num in enumerate(nums):
+           required = target - num
+           if required in num_to_index:
+               return [num_to_index[required], index]
+           num_to_index[num] = index
+       return []
+
+# Test cases
+print(twoSum([2, 7, 11, 15], 9))  # [0, 1]
+print(twoSum([3, 2, 4], 6))       # [1, 2]
+print(twoSum([3, 3], 6))          # [0, 1]
